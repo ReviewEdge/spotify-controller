1ad22a89960cd74e2a54d410f25aabeb0b5ed04f
Logan Richardson <reviewedge56@gmail.com>
2020-03-29 10:50:34 -0400

    Create config_example.py

---
+++ docs/config_example.py
@@ -0,0 +1,7 @@
+spotify_controller_info = {
+    "spotify_username": "XXXXX",
+    "spotify_dev_id": "XXXXX",
+    "spotify_dev_secret": "XXXXX",
+    "song_log_sheet_id": "XXXXX",
+    "blacklist_sheet_id": "XXXXX"
+}
