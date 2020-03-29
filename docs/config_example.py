f3dc5a62ff407ca3b351db317ea942e2488129b0
Logan Richardson <reviewedge56@gmail.com>
2020-03-29 11:03:44 -0400

    Update config_example.py

--- docs/config_example.py
+++ docs/config_example.py
@@ -1,7 +1,8 @@
-spotify_controller_info = {
-    "spotify_username": "XXXXX",
-    "spotify_dev_id": "XXXXX",
-    "spotify_dev_secret": "XXXXX",
-    "song_log_sheet_id": "XXXXX",
-    "blacklist_sheet_id": "XXXXX"
-}
+# When you create your own configuration file, make sure it is named "config.py"
+
+# spotify info:
+spotify_username = "robertstesting56"
+spotify_dev_id = "f2cad43bcc814bbf81981cb0fe50c503"
+spotify_dev_secret = "e28a78c3f82e4eef9a861fadfbda18a2"
+song_log_sheet_id = "16BXd0VBiF1qHAO2gCLSgwJlGT_I2mZENEdDnW2Eo5No"
+blacklist_sheet_id = "1ZAieNHkJ7dmYlxI36h-db_uYgHwMY11CPwBk6Xd-niw"
