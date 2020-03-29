35fea7e4a104eb328b07915d28cc9653140989ab
Logan Richardson <reviewedge56@gmail.com>
2020-03-29 11:16:29 -0400

    Delete config_example.py

--- docs/config_example.py
+++
@@ -1,8 +0,0 @@
-# When you create your own configuration file, make sure it is named "config.py"
-
-# spotify info:
-spotify_username = "XXXXX"
-spotify_dev_id = "XXXXX"
-spotify_dev_secret = "XXXXX"
-song_log_sheet_id = "XXXXX"
-blacklist_sheet_id = "XXXXX"
