983204efadf567d980ab91862200856c4afbeec9
ReviewEdge <reviewedge56@gmail.com>
2020-05-01 15:19:47 -0400

    deleted old comment

--- sample/gsheets_tool.py
+++ sample/gsheets_tool.py
@@ -24,8 +24,6 @@ def authenticate_sheets_api():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                # For a standard Raspbian setup, change path of client_secretes_file.json to:
-                # /home/pi/MyPiServer/server/spotify_controller_repo/sample/client_secrets_file.json
                 config.client_secrets_file_path, "https://www.googleapis.com/auth/spreadsheets")
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
